44d12f51584db01ad8fcc13dd528dc94bc5ca041
MadamGaBi <bila.galyna@gmail.com>
2015-09-24 19:54:22 +0300

    minor changes

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ database = [('Помідори', 5), ('Огірки', 7), ('Апельсини',
 @app.route("/food/", methods=['POST','GET'])
 def food():
 	if request.method == "POST":
-		flag = True
+		is_food_exists = True
 		for i in database:
 			if i[0] == str(request.form["name_of_food"]):
-				flag = False
+				is_food_exists = False
 				database[database.index(i)] = (str(request.form["name_of_food"]), ((i[1] + int(request.form["price_of_food"]))/2.0))
-		if flag:
+		if is_food_exists:
 			database.append((str(request.form["name_of_food"]), int(request.form["price_of_food"])))
 			
 	s = '<ul>\n'
@@ -23,12 +23,12 @@ def food():
 	s += '</ul>\n' + add_food_form() + change_price_form()
 	return s
 
-@app.route('/add_food/')
+
 def add_food_form():
 	form = '<form action="/food/" method="post">\n<fieldset>\n<legend>Add food:</legend>\n<br>\nName:<br>\n<input type="text" name="name_of_food" id="name_of_food" required>\n<br>\nPrice:<br>\n<input type="text" name="price_of_food" id="price_of_food" defoult=None>\n<br>\n<br>\n<input type="submit" value="Submit">\n</fieldset>\n</form>'
 	return form
 
-@app.route('/change_price/')
+
 def change_price_form():
 	form = '<form action="/food/" method="post">\n<fieldset>\n<legend>Change price:</legend>\n<br>\nSelect name:<br>\n<select name="name_of_food"><br>\n'
 	for i in database:
@@ -41,4 +41,5 @@ def send_js(path):
     return send_from_directory('www', path)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
+
